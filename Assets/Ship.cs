506d39ce83abe7bf457a03affa937b58d831a971
williams.matias@gmail.com <williams.matias@gmail.com@a5637aab-b8b5-6635-4f6a-4781d6682f5f>
2011-08-09 23:46:45 +0000

    aca van los prefabs

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 
 public class Ship : MonoBehaviour {
+	
+	public GameObject bullet;
+	
 	void Start () {
 		
 	}
 	
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			GameObject bulletGameObject = (GameObject)Instantiate(bullet);
+			Transform anotherTransform = bulletGameObject.transform;
+			anotherTransform.position = transform.position;
+			anotherTransform.rotation = transform.rotation;
+			//anotherTransform.rigidbody.velocity = transform.rigidbody.velocity;
+			anotherTransform.rigidbody.velocity = new Vector3(3F,0,0);
+			//anotherTransform.rigidbody.velocity.Scale(new Vector3(1.5F,1.5F,1.5F));
+		}
 	}
 }
