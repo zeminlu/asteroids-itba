b97923a779dab98aaebf78db47b8abff08abaaa6
zeminlu <zeminlu@a5637aab-b8b5-6635-4f6a-4781d6682f5f>
2011-08-11 00:32:27 +0000

    Aprolije varias cosas de la maniobrabilidad, agregue desaceleracion, el fuego es más copado ahora.

--- trunk/Assets/ScreenWrapper.cs
+++ trunk/Assets/ScreenWrapper.cs
@@ -7,9 +7,9 @@ public class ScreenWrapper : MonoBehaviour {
 	
 	void Awake () {
 		Vector3 wrapmin = Camera.main.ScreenToWorldPoint(new Vector3(0,0,Camera.main.transform.position.y));
-		wrap = new Vector2(wrapmin.x - 4, wrapmin.z - 4);
+		wrap = new Vector2(wrapmin.x - 1, wrapmin.z - 1);
 		Vector3 wrapmax = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,Camera.main.transform.position.y));
-		wrapMax = new Vector2(wrapmax.x + 4, wrapmax.z + 4);
+		wrapMax = new Vector2(wrapmax.x + 1, wrapmax.z + 1);
 	}
 	
 	void Update () {
